f1ae95397bf91de31db437b6736279dc951de90c
Delushaan <delushaan@outlook.com>
2024-03-19 22:07:58 -0400

    Imp: Category view/add/validation

---
+++ BulkWebApi/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkWebApi.Data;
+using BulkWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkWebApi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryDataList = _db.Categories.ToList();
+            return View(categoryDataList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder Cannot exaxtly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BulkWebApi/Migrations/20240320001423_SeedCategoryData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "Adventure" },
+                    { 3, 3, "Horror" },
+                    { 4, 4, "Sci-Fi" },
+                    { 5, 5, "Fantasy" },
+                    { 6, 6, "Drama" },
+                    { 7, 7, "Comedy" },
+                    { 8, 8, "Thriller" },
+                    { 9, 9, "Mystery" },
+                    { 10, 10, "Documentary" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
